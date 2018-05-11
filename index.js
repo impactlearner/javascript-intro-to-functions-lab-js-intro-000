2f25296a1b183a53a3715ec3111f9e270dc91195
impactlearner <time.darden@gmail.com>
2018-05-11 19:18:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
+var lowercase='hi, grandma'
 lowercase.toLowerCase()===lowercase
   return "I can't hear you!"
 }
